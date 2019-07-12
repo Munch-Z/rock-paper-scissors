ef7267c379be12a982aaafe924a734da9e8d0761
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-07-11 19:27:52 -0700

    Ties is not working exactly how I want it, but everything else is.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,13 +16,6 @@ const firebaseConfig = {
 //DOM Stuff
 
 const selectionsDiv = document.getElementById('selections');
-const player1PickDiv = document.getElementById('player1');
-const player2PickDiv = document.getElementById('player2');
-const player1WinsDiv = document.getElementById('player1Wins');
-const player1LossesDiv = document.getElementById('player1Losses');
-const player2WinsDiv = document.getElementById('player2Wins');
-const player2LossesDiv = document.getElementById('player2Wins');
-const tiesDiv = document.getElementById('ties');
 
 //test is a placeholder string because firebase is dumb and doesn't let me check for paths that are empty strings
 
@@ -49,7 +42,6 @@ connectedRef.on('value', (snap) => {
                     wins: 0,
                     losses: 0,
                     ties: 0,
-                    picked: false
                 });
                 host = true;
             } else {
@@ -61,8 +53,6 @@ connectedRef.on('value', (snap) => {
                     choice: "",
                     wins: 0,
                     losses: 0,
-                    ties: 0,
-                    picked: false
                 });
             }
         })
@@ -72,17 +62,12 @@ connectedRef.on('value', (snap) => {
 selectionsDiv.addEventListener('click', (e) => {
     const playerPicked = e.target.id;
 
-    console.log(host);
-
     myConnectionsRef.once('value').then((snap) => {
         if (snap.child('playerOne').exists() && snap.child('playerTwo').exists()) {
-            let player1picked = snap.child('playerOne').child('picked').val();
             if (host) {
                 myConnectionsRef.child('playerOne').child('choice').set(playerPicked);
-                myConnectionsRef.child('playerOne').child('picked').set(true);
             } else {
                 myConnectionsRef.child('playerTwo').child('choice').set(playerPicked);
-                myConnectionsRef.child('playerTwo').child('picked').set(true);
             }
 
             checkPicks();
@@ -91,10 +76,15 @@ selectionsDiv.addEventListener('click', (e) => {
 })
 
 function checkPicks() {
-    const alertZone = document.getElementById('alertZone');
-    console.log('Picks function fired')
+
+
 
     myConnectionsRef.once('value').then((snap) => {
+
+        //html stuff
+
+
+        //get values from DB
         let p1Pick = snap.child('playerOne').child('choice').val();
         let p2Pick = snap.child('playerTwo').child('choice').val();
         let p1Wins = snap.child('playerOne').child('wins').val();
@@ -102,27 +92,15 @@ function checkPicks() {
         let p1Losses = snap.child('playerOne').child('losses').val();
         let p2Losses = snap.child('playerTwo').child('losses').val();
         let p1Ties = snap.child('playerOne').child('ties').val();
-        let p2Ties = snap.child('playerTwo').child('ties').val();
-        console.log('Inside the promise');
 
-        if (p1Pick && p2Pick !== "") {
-            myConnectionsRef.child('playerOne').child('picked').set(true);
-            myConnectionsRef.child('playerTwo').child('picked').set(true);
-            console.log('Both picks were not strings')
+        if (p1Pick && p2Pick) {
+
 
             if (p1Pick === p2Pick) {
                 p1Ties++;
-                p2Ties++;
                 myConnectionsRef.child('playerOne').child('ties').set(p1Ties);
-                myConnectionsRef.child('playerTwo').child('ties').set(p2Ties);
                 myConnectionsRef.child('playerOne').child('choice').set("");
                 myConnectionsRef.child('playerTwo').child('choice').set("");
-                alertZone.textContent = 'Tie';
-                console.log('tie');
-
-                tiesDiv.textContent = 'Ties: ' + p1Ties;
-
-                tiesDiv.textContent = 'Ties ' + p2Ties;
             } else if (p1Pick === 'paper' && p2Pick === 'rock' || p1Pick === 'rock' && p2Pick === 'scissors' || p1Pick === 'scissors' && p2Pick === 'paper') {
                 p1Wins++;
                 p2Losses++;
@@ -130,31 +108,87 @@ function checkPicks() {
                 myConnectionsRef.child('playerTwo').child('losses').set(p2Losses);
                 myConnectionsRef.child('playerOne').child('choice').set("");
                 myConnectionsRef.child('playerTwo').child('choice').set("");
-                alertZone.textContent = 'Player 1 Wins';
-                console.log('Player 1 Wins');
-
-                player1WinsDiv.textContent = 'Player 1 Wins: ' + p1Wins;
-
-                player2LossesDiv.textContent = 'Player 1 Losses ' + p2Losses;
-
             } else {
-                console.log('Player 1 loses');
                 p1Losses++;
                 p2Wins++;
                 myConnectionsRef.child('playerOne').child('losses').set(p1Losses);
                 myConnectionsRef.child('playerTwo').child('wins').set(p2Wins);
                 myConnectionsRef.child('playerOne').child('choice').set("");
                 myConnectionsRef.child('playerTwo').child('choice').set("");
-                alertZone.textContent = 'Player 2 Wins';
-
-                player1LossesDiv.textContent = 'Player 1 Losses: ' + p1Losses;
-
-                player2WinsDiv.textContent = 'Player 2 Wins: ' + p2Wins;
-
-
             }
 
 
         }
     })
 }
+
+
+
+//playerOne listeners
+myConnectionsRef.child('playerOne').child('wins').on('value', (snap) => {
+    const alertZone = document.getElementById('alertZone');
+    const player1WinsDiv = document.getElementById('player1Wins');
+    let p1Wins = snap.val()
+
+    player1WinsDiv.textContent = 'Player 1 Wins: ' + p1Wins;
+    if (p1Wins > 0) {
+        alertZone.textContent = 'Player 1 Won!'
+    }
+})
+
+myConnectionsRef.child('playerOne').child('losses').on('value', (snap) => {
+    const player1LossesDiv = document.getElementById('player1Losses');
+    let p1Losses = snap.val()
+    player1LossesDiv.textContent = 'Player 1 Losses: ' + p1Losses;
+})
+
+myConnectionsRef.child('playerOne').child('choice').on('value', (snap) => {
+    let pick = snap.val();
+    const player1PickDiv = document.getElementById('player1');
+    if (pick) {
+        player1PickDiv.textContent = 'Player 1 picked: ' + pick.toUpperCase();
+    }
+
+})
+
+
+
+//playerTwo listeners
+myConnectionsRef.child('playerTwo').child('wins').on('value', (snap) => {
+    let p2Wins = snap.val()
+    const player2WinsDiv = document.getElementById('player2Wins');
+    const alertZone = document.getElementById('alertZone');
+
+    player2WinsDiv.textContent = 'Player 2 Wins: ' + p2Wins;
+    if (p2Wins > 0) {
+        alertZone.textContent = 'Player 2 Won!'
+    }
+
+})
+
+myConnectionsRef.child('playerTwo').child('losses').on('value', (snap) => {
+    const player2LossesDiv = document.getElementById('player2Wins');
+    let p2Losses = snap.val()
+    player2LossesDiv.textContent = 'Player 2 Wins: ' + p2Losses;
+})
+
+myConnectionsRef.child('playerTwo').child('choice').on('value', (snap) => {
+    const player2PickDiv = document.getElementById('player2');
+    let pick = snap.val();
+    if (pick) {
+        player2PickDiv.textContent = 'Player 2 picked: ' + pick.toUpperCase();
+    }
+
+})
+
+//Ties
+myConnectionsRef.child('playerOne').child('ties').on('value', (snap) => {
+    let ties = snap.val()
+    const tiesDiv = document.getElementById('ties');
+    const alertZone = document.getElementById('alertZone');
+
+    tiesDiv.textContent = 'Ties: ' + ties;
+    if (ties > 0) {
+        alertZone.textContent = 'It\'s a tie!';
+    }
+})
